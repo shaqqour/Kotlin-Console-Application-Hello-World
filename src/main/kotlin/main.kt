d10190ebac81d3cbc619f2d1d7a666841366ba9e
Shaqqour <mshaqqourm@gmail.com>
2020-11-15 11:42:16 +0400

    Switch to when expression to see either welcome the user or kick them out

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
 fun main(args: Array<String>) {
     println("What's your name?")
     val name = readLine()
-    if (name == "Shaqqour") {
-        println("Hello $name!")
-    } else
-    {
-        println("You are not welcome here!")
+    when (name) {
+        "Shaqqour" -> println("You are so welcome $name. We are glad to have you with us!")
+        "Ali" -> println("Hello $name!")
+        "Hiba" -> println("Hello $name!")
+        else -> println("You are not welcome here!")
     }
 }
