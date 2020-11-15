ad47a982f100a582facb51c9b3a5d7a298c76438
Shaqqour <mshaqqourm@gmail.com>
2020-11-15 12:23:48 +0400

    Asks the user for the age then uses when expression to detrmine the user age category

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,14 @@
 fun main(args: Array<String>) {
-    println("What's your name?")
-    val name = readLine()
-    when (name) {
-        "Shaqqour" -> println("You are so welcome $name. We are glad to have you with us!")
-        "Ali", "Hiba" -> println("Hello $name!")
+    println("What's your age?")
+    val age = readLine()
+    val myStringNumber: String = age.toString();
+    val number: Int = myStringNumber.toInt()
+    println(number)
+    when (number) {
+        in 1..3 -> println("You are a toddler!")
+        in 4..10 -> print("You are a kid!")
+        in 11..17 -> print("You are a teenager!")
+        in 18..100 -> print("You are an adult!")
         else -> println("You are not welcome here!")
     }
 }
