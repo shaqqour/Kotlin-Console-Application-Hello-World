6648c6825c71f6c57f7ba3b06449a8816dd1d0d9
Shaqqour <mshaqqourm@gmail.com>
2020-11-15 11:31:33 +0400

    Writes a condition to either welcome the user or turn them back

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
     println("What's your name?")
     val name = readLine()
-    println("Hello $name!")
+    if (name == "Shaqqour") {
+        println("Hello $name!")
+    } else
+    {
+        println("You are not welcome here!")
+    }
 }
